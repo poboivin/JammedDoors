40a171150f35f9e9a773c92c8108738385ae0b62
SJH607-09W\Graphic <poboivin2@gmail.com>
2018-10-10 23:10:21 -0400

    added standard assets

---
+++ Jammed Doors/Assets/Scripts/LerpUtility.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpUtility {
+
+    public static float EaseOut(float currentLerpTime, float lerpTime)
+    {
+        float t = currentLerpTime / lerpTime;
+        t = Mathf.Sin(t * Mathf.PI * 0.5f);
+        return t;
+    }
+    public static float EaseIn(float currentLerpTime, float lerpTime)
+    {
+        float t = currentLerpTime / lerpTime;
+        t = 1f - Mathf.Cos(t * Mathf.PI * 0.5f);
+        return t;
+    }
+    public static float Exponential(float currentLerpTime, float lerpTime)
+    {
+        float t = currentLerpTime / lerpTime;
+        t = t * t;
+        return t;
+    }
+    public static float Smooth(float currentLerpTime, float lerpTime)
+    {
+        float t = currentLerpTime / lerpTime;
+        t = t * t * (3f - 2f * t);
+        return t;
+    }
+    public static float Smoother(float currentLerpTime, float lerpTime)
+    {
+        float t = currentLerpTime / lerpTime;
+        t = t * t * (3f - 2f * t);
+        return t;
+    }
+    public static Vector3 SineWave(float time, Vector3 pos, Vector3 dir, float amplitude = 1f, float period = 1f)
+    {
+        float theta = time / period;
+        float distance = amplitude * Mathf.Sin(theta);
+        return pos + dir * distance;
+    }
+}
---
+++ Jammed Doors/Assets/Scripts/dissoveLerp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dissoveLerp : MonoBehaviour {
+    Material mat;
+    public float lerpTime = 1f;
+    float currentLerpTime;
+    public bool Lerp;
+    // Use this for initialization
+    private void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Lerp)
+        {
+            currentLerpTime += Time.deltaTime;
+            if (currentLerpTime > lerpTime)
+            {
+
+                Lerp = false;
+            }
+            float perc = LerpUtility.Smoother(currentLerpTime, lerpTime);
+            mat.SetFloat("_DissolveAmount",1- perc);
+        }
+     
+        
+    }
+}
